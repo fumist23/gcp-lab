ed5088b85c77350a8c00af29c2fd97b7829b902f
fumist23 <fumist1028@gmail.com>
2022-06-19 21:15:06 +0900

    :sparkles: feat: google_artifact_registry_repository_iam_member.public

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -5,3 +5,12 @@ resource "google_artifact_registry_repository" "public" {
   format        = "DOCKER"
   location      = "asia-northeast1"
 }
+
+resource "google_artifact_registry_repository_iam_member" "public" {
+    provider = google-beta
+
+    location = google_artifact_registry_repository.public.location
+    repository = google_artifact_registry_repository.public.repository_id
+    role = "roles/viewer"
+    member = "allUsers"
+}
